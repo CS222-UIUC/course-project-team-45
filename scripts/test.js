1c68689058015a6dad0f9eeb8a94121c75a4fcba
DanielLi0 <70663778+DanielLi0@users.noreply.github.com>
2022-09-26 19:17:33 -0500

    Try #5

--- scripts/test.js
+++ scripts/test.js
@@ -2,3 +2,8 @@ function test() {
     console.log("CAN YOU SEE THIS?!");
     document.getElementById("Test").innerHTML = "TEST APPLIED.";
 }
+
+function getClassData() {
+    fetch("https://courses.illinois.edu/cisapi/schedule/courses?year=2012&term=spring§ionTypeCode=LEC§ionTypeCode=Q&collegeCode=KV&creditHours=3&subject=CHEM&sessionId=1&gened=NAT&qp=atomic+structure")
+    .console("Successful");
+}
