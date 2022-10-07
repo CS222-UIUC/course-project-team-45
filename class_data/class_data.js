83ccd3f63130011da6f7c1afe233b8635535817d
DanielLi0 <70663778+DanielLi0@users.noreply.github.com>
2022-10-07 13:31:52 -0500

    First stage: Successful fetch and read

--- class_data/class_data.js
+++ class_data/class_data.js
@@ -1,36 +1,69 @@
 /*
 *   This function calls and returns the data from the API
-*   This function assumes the parameters are in the format string, number
+*   This function assumes the parameters are in the format string, number, string
+*   Assumes the year parameter is formatted correctly for comparison ("Spring 2022", for example)
 */
-function getClassData(subject, number) {
-    let base_url = 'https://uiuc-course-api.herokuapp.com/api/classes/?subject=';
+function getClassData(subject, number, year) {
+    let base_url = 'https://cors-anywhere.herokuapp.com/https://uiuc-course-api.herokuapp.com/api/classes/?subject=';
     base_url += subject.toUpperCase();
     base_url += '&number=';
     base_url += String(number);
     console.log(base_url);
-    
-    fetch(base_url, {mode: 'cors'})
-    .then((response) => {
-        console.log(response);
-        return response.text(); })
-    .then((unparsed) => {
-        console.log(unparsed);
-        let data = unparsed ? JSON.parse(unparsed) : {};
-        document.getElementById("Test").innerHTML = data.name;
-    });
-    
-   /*
+    /*
     var xhr = new XMLHttpRequest();
-    
-    xhr.open('GET', base_url, true);
-    xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
-    xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+    xhr.open('GET', base_url);
     xhr.send();
-    xhr.onload = function() {
-        var jsonResponse = xhr.response;
-        document.getElementById("Test").innerHTML = jsonResponse.name;
-    }
+    
+    xhr.onload = 
     */
-    
-    
+    fetch(base_url).then((response) =>
+        response = response.json().then((jsonResponse) => {
+            let class_data = new Array();
+
+            for (let i = 0; i < jsonResponse.length; i++) {
+                let sections = jsonResponse[i];
+                if (sections.yearterm == year) {
+                    let section = new ClassSection(sections.building, sections.room, sections.days_of_week,
+                                                sections.start_time, sections.end_time, sections.section,
+                                                sections.type, sections.type_code, sections.label);
+                    console.log(section, i);
+                    class_data.push(section);
+                }
+            }
+            console.log(class_data.toString());
+            document.getElementById("Test").innerHTML = class_data[0].label;
+        })
+    ); 
+}
+
+function ClassSection(...data) {
+    //Data members
+    this.building = data[0];
+    this.room = data[1];
+    this.days_of_week = data[2];
+    this.start_time = data[3];
+    this.end_time = data[4];
+    this.section = data[5];
+    this.type = data[6];
+    this.type_code = data[7];
+    this.label = data[8];
+
+    //Potentially useful methods
+    //Returns building and room in a pair
+    this.getLocation = function() {
+        return new Array(this.building, this.room);
+    }
+    //Provides the start, endtime, and days as an array
+    this.getTime = function() {
+        return new Array(this.start_time, this.end_time, this.days_of_week);
+    }
+    //Provides class data about the section
+    this.getType = function() {
+        return new Array(this.label, this.type, this.type_code, this.section);
+    }
+    //If you don't need these methods you can just call the property name
+}
+
+function saveData(class_data) {
+
 }
