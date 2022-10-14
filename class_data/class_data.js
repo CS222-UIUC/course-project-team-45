b316e6c659ef8a6f2848558a1e627b6a9950cc00
DanielLi0 <70663778+DanielLi0@users.noreply.github.com>
2022-10-14 10:24:13 -0500

    Fixed standardJS style errors

--- class_data/class_data.js
+++ class_data/class_data.js
@@ -1,33 +1,41 @@
-//Global class data array (THIS ONLY EXISTS PER WEBPAGE)
-//Resides in window.CLASS_DATA
-var CLASS_DATA = new Array();
+// Global class data array (THIS ONLY EXISTS PER WEBPAGE)
+// Resides in window.CLASS_DATA
+const CLASS_DATA = []
 /*
 *   This function calls and returns the data from the API
 *   This function assumes the parameters are in the format string, number, string
 *   Assumes the year parameter is formatted correctly for comparison ("Spring 2022", for example)
 */
-function loadClassData(subject, number, year) {
-    let base_url = 'https://uiuc-course-api.herokuapp.com/api/classes/?subject=';
-    base_url += subject.toUpperCase();
-    base_url += '&number=';
-    base_url += String(number);
-    
-    fetch(base_url).then((response) =>
-        response = response.json().then((jsonResponse) => {
-            for (let i = 0; i < jsonResponse.length; i++) {
-                let sections = jsonResponse[i];
-                if (sections.yearterm == year) {
-                    const section = {building: sections.building, room: sections.room, days_of_week: sections.days_of_week,
-                                     start_time: sections.start_time, end_time: sections.end_time, section: sections.section,
-                                     type: sections.type, type_code: sections.type_code, label: sections.label};
-                    CLASS_DATA.push(section);
-                    
-                }
-            }
-            localStorage.setItem("CLASS_DATA", JSON.stringify(CLASS_DATA));
-        })
-    );
-    console.log(CLASS_DATA);
+// eslint-disable-next-line no-unused-vars
+function loadClassData (subject, number, year) {
+  let baseUrl = 'https://uiuc-course-api.herokuapp.com/api/classes/?subject='
+  baseUrl += subject.toUpperCase()
+  baseUrl += '&number='
+  baseUrl += String(number)
+  // eslint-disable-next-line no-return-assign
+  fetch(baseUrl).then((response) =>
+    response = response.json().then((jsonResponse) => {
+      for (let i = 0; i < jsonResponse.length; i++) {
+        const sections = jsonResponse[i]
+        if (sections.yearterm === year) {
+          const section = {
+            building: sections.building,
+            room: sections.room,
+            days_of_week: sections.days_of_week,
+            start_time: sections.start_time,
+            end_time: sections.end_time,
+            section: sections.section,
+            type: sections.type,
+            type_code: sections.type_code,
+            label: sections.label
+          }
+          CLASS_DATA.push(section)
+        }
+      }
+      window.localStorage.setItem('CLASS_DATA', JSON.stringify(CLASS_DATA))
+    })
+  )
+  console.log(CLASS_DATA)
 }
 /*
 The object in the array has the following properties:
@@ -41,6 +49,6 @@ Property Name
 - type
 - type_code
 - label
-You can call the property name to obtain the value as these are in (key, value) pairs 
-All values are strings 
+You can call the property name to obtain the value as these are in (key, value) pairs
+All values are strings
 */
--- class_data/test_global.js
+++ class_data/test_global.js
@@ -1,18 +1,20 @@
-function listSections() {
-    let data = localStorage.getItem("CLASS_DATA");
-    CLASS_DATA = JSON.parse(data);
-    console.log(CLASS_DATA);
-    let list = document.getElementById("list");
-    document.createElement("p").id = "title";
-    document.getElementById("title").innerText = CLASS_DATA[0].label;
+// eslint-disable-next-line no-unused-vars
+function listSections () {
+  const data = window.localStorage.getItem('CLASS_DATA')
+  window.CLASS_DATA = JSON.parse(data)
+  console.log(window.CLASS_DATA)
+  const list = document.getElementById('list')
+  document.createElement('p').id = 'title'
+  document.getElementById('title').innerText = window.CLASS_DATA[0].label
 
-    for (let section of CLASS_DATA) {
-        let li = document.createElement("li");
-        li.innerHTML += `${section.type}<ul><li>Building: ${section.building}</li><li>Room: ${section.room}</li></ul>`;
-        list.appendChild(li);
-    }
+  for (const section of window.CLASS_DATA) {
+    const li = document.createElement('li')
+    li.innerHTML += `${section.type}<ul><li>Building: ${section.building}</li><li>Room: ${section.room}</li></ul>`
+    list.appendChild(li)
+  }
 }
 
-function callData() {
-    console.log(CLASS_DATA);
+// eslint-disable-next-line no-unused-vars
+function callData () {
+  console.log(window.CLASS_DATA)
 }
