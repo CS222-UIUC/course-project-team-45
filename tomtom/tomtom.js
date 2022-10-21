d928b860a37f207b8263ef74393a245ebb5f9774
Tiffany <tyu19@illinois.edu>
2022-10-20 23:05:43 -0500

    Modified getTomTomDirections

--- tomtom/tomtom.js
+++ tomtom/tomtom.js
@@ -23,17 +23,16 @@ async function getTomTomDirections() {
     let long1 = '13.42936';
     let lat2 = '52.50274';
     let long2 = '13.43872'
-    let loc1 = lat1 + ',' + long1;
-    let loc2 = lat2 + ',' + long2;
-    let location = loc1 + ":" + loc2 + "/";
+    let loc1 = lat1 + '%2C' + long1;
+    let loc2 = lat2 + '%2C' + long2;
+    let location = loc1 + "%3A" + loc2;
     baseUrl += location;
-    baseUrl += 'json?instructionsType=text&sectionType=pedestrian&routeType=shortest&travelMode=pedestrian&key=AvmH2sk25s5jn09Mhc980ITYPNpfAm31';
+    baseUrl += '/json?instructionsType=text&sectionType=pedestrian&report=effectiveSettings&routeType=shortest&travelMode=pedestrian&key=AvmH2sk25s5jn09Mhc980ITYPNpfAm31';
     //url is ready for request
     await fetch(baseUrl).then((response) =>
     response = response.json().then((jsonResponse) => {
-      let latlong = JSON.parse(window.localStorage.getItem('LAT_LONG')) // taken from getLatLong
-      let instructions = jsonResponse["instructionGroups"]["groupMessage"];
-      console.log(instructions);
+      let instructions = jsonResponse.guidance.instructionGroups.groupMessage;
+      console.log(instructions); // Outputs detailed directions to destination
     })
   )
     //document.getElementById("test").innerHTML = baseUrl;//whatever the output is
