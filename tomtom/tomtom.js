f238aacab76632412c134fdc8c0b7af6fc1ec93b
Tiffany <tyu19@illinois.edu>
2022-10-27 23:18:26 -0500

    Added link to map page

--- tomtom/tomtom.js
+++ tomtom/tomtom.js
@@ -1,10 +1,13 @@
+var APIKEY = "AvmH2sk25s5jn09Mhc980ITYPNpfAm31";
+
 async function getLatLong(building) { 
   // Geocoding using tomtom Place API
   // May need to manually type out addresses for each building for this to work
   let baseUrl = 'https://api.tomtom.com/search/2/search/';
   let place = encodeURI(building).replace("'", "%27"); // URL encode building name
   baseUrl += place;
-  baseUrl += '.json?lat=40.1020&lon=-88.2272&radius=5000&minFuzzyLevel=1&maxFuzzyLevel=2&view=Unified&relatedPois=off&key=AvmH2sk25s5jn09Mhc980ITYPNpfAm31';
+  baseUrl += '.json?lat=40.1020&lon=-88.2272&radius=5000&minFuzzyLevel=1&maxFuzzyLevel=2&view=Unified&relatedPois=off&key=';
+  baseUrl += APIKEY;
   await fetch(baseUrl).then((response) =>
     response = response.json().then((jsonResponse) => {
       const lat = jsonResponse.results[0].position.lat // Gets the first result's latitude
@@ -27,7 +30,8 @@ async function getTomTomDirections() {
     let loc2 = lat2 + '%2C' + long2;
     let location = loc1 + "%3A" + loc2;
     baseUrl += location;
-    baseUrl += '/json?instructionsType=text&sectionType=pedestrian&report=effectiveSettings&routeType=shortest&travelMode=pedestrian&key=AvmH2sk25s5jn09Mhc980ITYPNpfAm31';
+    baseUrl += '/json?instructionsType=text&sectionType=pedestrian&report=effectiveSettings&routeType=shortest&travelMode=pedestrian&key=';
+    baseUrl += APIKEY;
     //url is ready for request
     await fetch(baseUrl).then((response) =>
     response = response.json().then((jsonResponse) => {
