419403db04f8b7d3b0fae968d5bb67bccbf8109f
Tiffany <tyu19@illinois.edu>
2022-11-27 22:30:03 -0600

    Fixed map display bug

--- tomtom/tomtom.js
+++ tomtom/tomtom.js
@@ -11,35 +11,21 @@ var map = tt.map({
 });
 
 var markers = []; // Stores our markers
-var num_clicks = 0; // Keeps track of num of clicks and helps delete route layer ;-;
-
-// Does the moving map animation when you insert a location
-// function moveMap() {
-//   var lng = 0;
-//   var lat = 0;
-//   for(mark of markers) {
-//       lng += mark.getLngLat()[0];
-//       lat += mark.getLngLat()[1];
-//   }
-//   lng = lng / markers.length;
-//   lat = lat / markers.length;
-
-//   map.flyTo({
-//     center: [lng,lat],
-//     zoom: 18
-//   });
-// }
 
 // Resets the map / schedule display if you click another button
 function clear_() {
   document.getElementById('classlist').innerHTML = '';
   document.getElementById('distance').innerHTML = '';
   document.getElementById('time').innerHTML = '';
-  map.removeLayer('route' + num_clicks.toString());
+  // Deletes marker data after plotting Route
+  // for (mark of markers) {
+  //   mark.remove();
+  // }
+  // markers = [];
 }
 
-// Handles search results
-function handleResults (result) {
+// Adds location to map
+function handleResults(result) {
   console.log(result)
   if (result.results) {
     // Creates a marker on lnglat
@@ -51,7 +37,7 @@ function handleResults (result) {
 }
 
 // Helps find location data
-function search (address) {
+function search(address) {
   tt.services.fuzzySearch({
     key: APIKEY,
     query: address,
@@ -61,20 +47,20 @@ function search (address) {
 
 function displayRoute(geoJSON) {
   routeLayer = map.addLayer({
-    // Created a unique id bc deleting it is a pain
-    'id' : 'route' + num_clicks.toString(),
-    'type' : 'line',
-    'source' : {
-      'type' : 'geojson',
-      'data' : geoJSON
+    id: 'route',
+    type: 'line',
+    source: {
+      type: 'geojson',
+      data: geoJSON
     },
-    'paint' : {
-      'line-color' : 'blue',
-      'line-width' : 4,
+    paint: {
+      'line-color': 'blue',
+      'line-width': 5
     }
-  });
+  })
 }
 
+var doubleClick = false;
 // Creates route from building address list
 function createRoute() {
   var routeOptions = {
@@ -88,14 +74,17 @@ function createRoute() {
   tt.services.calculateRoute(routeOptions).go().then(
     function(routeData) {
       // Displays your distance and time
-      document.getElementById('distance').innerHTML = 
-        "Total distance: " 
-        + (routeData.routes[0].summary.lengthInMeters * 0.000621371).toFixed(2)
-        + " miles";
-      document.getElementById('time').innerHTML = 
-        "Approximate Time: " 
-        + (routeData.routes[0].summary.travelTimeInSeconds / 60).toFixed(2)
-        + " min";
+      if (!doubleClick) {
+        document.getElementById('distance').innerHTML = 
+          "Total distance: " 
+          + (routeData.routes[0].summary.lengthInMeters * 0.000621371).toFixed(2)
+          + " miles";
+        document.getElementById('time').innerHTML = 
+          "Approximate Time: " 
+          + (routeData.routes[0].summary.travelTimeInSeconds / 60).toFixed(2)
+          + " min";
+        doubleClick = true;
+      }
 
       console.log(routeData);
       // Geolocates our lnglat
@@ -115,41 +104,27 @@ function displaySchedule(section) {
   return element
 }
 
-function displayScheduleRoute(day) {
+function plotMap(day) {
+  if (doubleClick) { location.reload(); }
+  
   // Resets classes, distance, and time
   clear_();
-  num_clicks ++;
-
-  const schedule = JSON.parse(window.localStorage.getItem('SCHEDULE'));
-
-  let num_Classes = 0;
-
-  for (let i = 0; i < schedule.length; i++) {
-    if (schedule[i].days_of_week.includes(day)) {
-      // Displays your classes
-      document.getElementById('classlist').innerHTML += displaySchedule(schedule[i]);
-      // Searches and plot the class location
-      let building = schedule[i].building.toUpperCase();
-      let address = addressMap.get(building);
-      // Finds the lng and lat of the building and stores them
-      search(address);
-
-      num_Classes ++;
-    }
-  }
-  if (num_Classes == 0) {
+  const schedule = JSON.parse(window.localStorage.getItem('SORTED_SCHEDULE'))[day];
+  if (schedule.length == 0) {
     document.getElementById('classlist').innerHTML = '<p>You have no classes on this day! :D</p>';
     return;
   }
-  // Displays the route corresponding to each day
-  createRoute();
-  
-  // Moves map based on the average lnglat of each location
-  // moveMap();
-  
-  // Deletes marker data after plotting Route
-  for (mark of markers) {
-    mark.remove();
+
+  for (sched of schedule) {
+    // Displays your classes
+    if (!doubleClick) {
+      document.getElementById('classlist').innerHTML += displaySchedule(sched);
+    }
+    // Searches and plot the class location
+    let building = sched.building.toUpperCase();
+    let address = addressMap.get(building);
+    // Finds the lng and lat of the building and stores them
+    search(address);
+    createRoute();
   }
-  markers = [];
 }
